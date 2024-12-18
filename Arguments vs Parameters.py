01c8ba886e34225d66eb04f0615a04f6f85c003c
Javi3166 <javi3166@yahoo.com>
2024-12-18 00:22:20 -0500

    Added code

---
+++ Arguments vs Parameters.py
@@ -0,0 +1,4 @@
+def print_name(name): # <- name here is the parameter
+    print(name)
+
+print_name('Emilio') # <- 'Emilio' here is an argument
---
+++ Positional arguments.py
@@ -0,0 +1,6 @@
+def foo(a, b, c):
+    print(a, b, c)
+
+foo(1, 2, 3)
+
+# The position of the arguments will decide where they will go in the arguments list implicitly
